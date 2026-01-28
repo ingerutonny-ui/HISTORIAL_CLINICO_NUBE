8377295743ad2d7c283e76dcaa28e7330bb6d3b7
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-28 13:20:36 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,28 +1,27 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+# Importación absoluta para el entorno de Vercel
+from api import crud, models, schemas, database
 
-# Iniciamos la aplicación
 app = FastAPI()
 
-# Dependencia para obtener la sesión de la base de datos
 def get_db():
-    db = SessionLocal()
+    db = database.SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-@app.get("/")
+# La ruta debe ser /api/ porque así lo definimos en vercel.json
+@app.get("/api/")
 def read_root():
     return {"message": "API de Historial Clínico en la Nube funcionando"}
 
-@app.post("/pacientes/", response_model=schemas.Paciente)
+@app.post("/api/pacientes/", response_model=schemas.Paciente)
 def crear_nuevo_paciente(paciente: schemas.PacienteCreate, db: Session = Depends(get_db)):
     return crud.crear_paciente(db=db, paciente=paciente)
 
-@app.get("/pacientes/", response_model=List[schemas.Paciente])
+@app.get("/api/pacientes/", response_model=List[schemas.Paciente])
 def listar_pacientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.obtener_pacientes(db, skip=skip, limit=limit)
