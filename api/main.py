ad6d25fc918634422f1dd290415b1e97125482cd
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-29 18:55:27 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,21 +1,40 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from api.database import SessionLocal # Importación absoluta
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from api.database import SessionLocal, engine, Base
+from api import models, schemas
+
+# Crea las tablas si no existen
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Proyecto HISTORIAL_CLINICO_NUBE en linea"}
-
-@app.get("/healthz")
-def health_check():
+# Función para obtener la base de datos
+def get_db():
     db = SessionLocal()
     try:
-        # Verificamos la conexión a PostgreSQL
-        db.execute(text("SELECT 1"))
-        return {"status": "ok", "database": "connected"}
-    except Exception as e:
-        return {"status": "error", "details": str(e)}
+        yield db
     finally:
         db.close()
+
+@app.get("/")
+def read_root():
+    return {"message": "API HISTORIAL_CLINICO_NUBE en linea"}
+
+@app.post("/pacientes/", response_model=schemas.PacienteResponse)
+def crear_paciente(paciente: schemas.PacienteCreate, db: Session = Depends(get_db)):
+    # 1. Crear la instancia del modelo con los datos recibidos
+    nuevo_paciente = models.Paciente(
+        nombres=paciente.nombres,
+        apellidos=paciente.apellidos,
+        documento_identidad=paciente.documento_identidad
+    )
+    
+    # 2. Generar el código automático (ej: RA6855)
+    nuevo_paciente.codigo_paciente = nuevo_paciente.generar_codigo()
+    
+    # 3. Guardar en PostgreSQL
+    db.add(nuevo_paciente)
+    db.commit()
+    db.refresh(nuevo_paciente)
+    
+    return nuevo_paciente
