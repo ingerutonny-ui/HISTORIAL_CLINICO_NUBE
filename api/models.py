a9bab1b12bac7c5bde2204fb13893795d33d9473
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-28 20:56:01 -0400

    Create models.py

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Paciente(Base):
+    __tablename__ = "pacientes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    apellido = Column(String, index=True)
+    dni = Column(String, unique=True, index=True)
+    fecha_ingreso = Column(String)
+    codigo = Column(String, unique=True)
