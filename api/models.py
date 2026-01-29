cfc92839840dabdf5c15bd8d1108719d1786f6b3
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-28 21:16:58 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -7,6 +7,6 @@ class Paciente(Base):
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, index=True)
     apellido = Column(String, index=True)
-    dni = Column(String, unique=True, index=True)
+    ci = Column(String, unique=True, index=True)
     fecha_ingreso = Column(String)
     codigo = Column(String, unique=True)
