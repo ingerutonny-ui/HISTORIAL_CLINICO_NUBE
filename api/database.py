c262545b25a18a7f1f0e2f64bf28594deeb308d8
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-28 20:40:39 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,13 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import datetime
 
 # En Vercel, el único lugar con permiso de escritura es /tmp/
-SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/pacientes.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+class Paciente(Base):
+    __tablename__ = "pacientes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    apellido = Column(String, index=True)
+    dni = Column(String, unique=True, index=True)
+    fecha_ingreso = Column(String)
+    codigo = Column(String, unique=True)
+
+# Crear la tabla si no existe
+Base.metadata.create_all(bind=engine)
