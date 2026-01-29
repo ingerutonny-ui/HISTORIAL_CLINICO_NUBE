7e43a638a1d016cfbceff8d42a64bd904d3d2b56
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-29 17:13:04 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -10,7 +10,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Crear el motor de conexión
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 # Crear la sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
