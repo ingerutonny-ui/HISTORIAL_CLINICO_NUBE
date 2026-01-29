c6c03ba7a1d36007813abab1fc8fabb0685f9c06
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-29 11:51:30 -0400

    Update models.py
    fix: cambio temporal de nombre de tabla para evitar conflicto en Render

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class Paciente(Base):
-    __tablename__ = "pacientes"
+    __tablename__ = "pacientes_v2"
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True, index=True)
