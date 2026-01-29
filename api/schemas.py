6738c08bf5f68a3142515dc78bd56d2ef0f0d304
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-28 21:22:48 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,23 +1,14 @@
 from pydantic import BaseModel
-from datetime import datetime
-from typing import Optional
 
-# Esquema base para compartir campos comunes
 class PacienteBase(BaseModel):
     nombre: str
     apellido: str
-    historia_clinica: Optional[str] = None
+    ci: str
+    fechaIngreso: str
+    codigo: str
 
-# Esquema para la creación (lo que recibimos del Frontend)
-class PacienteCreate(PacienteBase):
-    # El codigo_paciente se generará en la lógica antes de guardar
-    pass
-
-# Esquema para la respuesta (lo que el API devuelve)
-class Paciente(PacienteBase):
+class PacienteResponse(PacienteBase):
     id: int
-    codigo_paciente: str
-    fecha_registro: datetime
 
     class Config:
-        from_attributes = True
+        orm_mode = True
