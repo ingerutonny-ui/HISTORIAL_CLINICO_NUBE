363007a470e90a11e8eedf1277fc1f61c3ebd2aa
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-30 19:29:31 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -5,14 +5,16 @@ from typing import List
 from . import models, schemas, crud
 from .database import engine, SessionLocal
 
+# Inicialización de base de datos
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# LA SOLUCIÓN REAL AL FALLO DE RED:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -25,8 +27,8 @@ def get_db():
         db.close()
 
 @app.get("/")
-def health():
-    return {"status": "ok"}
+def root():
+    return {"status": "online"}
 
 @app.get("/pacientes", response_model=List[schemas.Paciente])
 def read_pacientes(db: Session = Depends(get_db)):
@@ -36,5 +38,5 @@ def read_pacientes(db: Session = Depends(get_db)):
 def create_paciente(paciente: schemas.PacienteCreate, db: Session = Depends(get_db)):
     db_paciente = crud.get_paciente_by_ci(db, ci=paciente.documento_identidad)
     if db_paciente:
-        raise HTTPException(status_code=400, detail="Ya existe")
+        raise HTTPException(status_code=400, detail="Documento ya registrado")
     return crud.create_paciente(db=db, paciente=paciente)
