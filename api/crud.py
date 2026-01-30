822bea60faa7ea5abe0a3f5fafba24cf05dc257c
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-30 18:14:43 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -1,24 +1,24 @@
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 
-# Crear un nuevo paciente
-def crear_paciente(db: Session, nombre: str, apellido: str, ci: str, fecha_ingreso: str, codigo: str):
-    nuevo_paciente = models.Paciente(
-        nombre=nombre,
-        apellido=apellido,
-        ci=ci,
-        fecha_ingreso=fecha_ingreso,
-        codigo=codigo
+# Función para el botón CONSULTAR
+def get_pacientes(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Paciente).offset(skip).limit(limit).all()
+
+# Función para verificar si el paciente ya existe por CI
+def get_paciente_by_ci(db: Session, ci: str):
+    return db.query(models.Paciente).filter(models.Paciente.documento_identidad == ci).first()
+
+# Función para el botón REGISTRAR
+def create_paciente(db: Session, paciente: schemas.PacienteCreate):
+    # Creamos el objeto con los datos básicos que vienen del primer formulario
+    db_paciente = models.Paciente(
+        nombres=paciente.nombres,
+        apellidos=paciente.apellidos,
+        documento_identidad=paciente.documento_identidad,
+        codigo_paciente=paciente.codigo_paciente
     )
-    db.add(nuevo_paciente)
+    db.add(db_paciente)
     db.commit()
-    db.refresh(nuevo_paciente)
-    return nuevo_paciente
-
-# Consultar todos los pacientes
-def obtener_pacientes(db: Session):
-    return db.query(models.Paciente).all()
-
-# Consultar un paciente por CI
-def obtener_paciente_por_ci(db: Session, ci: str):
-    return db.query(models.Paciente).filter(models.Paciente.ci == ci).first()
+    db.refresh(db_paciente)
+    return db_paciente
