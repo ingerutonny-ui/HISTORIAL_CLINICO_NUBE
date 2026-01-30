dc9be5a35f143136d3760bef5d07f84bd8211df7
ingerutonny-ui <ingerutonny@gmail.com>
2026-01-30 15:13:36 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,22 +1,17 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
-from datetime import date
 
-# Lo que el Frontend envía (Formulario Inicial)
-class PacienteCreate(BaseModel):
+class PacienteBase(BaseModel):
     nombres: str
     apellidos: str
     documento_identidad: str
 
-# Lo que el Backend responde (Incluye el código generado)
-class PacienteResponse(BaseModel):
+class PacienteCreate(PacienteBase):
+    pass
+
+class Paciente(PacienteBase):
     id: int
     codigo_paciente: str
-    fecha_ingreso: datetime
-    nombres: str
-    apellidos: str
-    documento_identidad: str
-
     class Config:
         from_attributes = True
