abc1a8c137dab39851e47a3fecc5568677ba7b6f
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-04 14:08:11 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -2,11 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL limpia, sin parámetros extra
+# URL de tu base de datos (se mantiene igual)
 SQLALCHEMY_DATABASE_URL = "postgresql://historial_clinico_nube_user:Y0yL3U0N56j0qS0u5hL936M2yK6vS9rX@dpg-cub9o093967s73d1u83g-a.oregon-postgres.render.com/historial_clinico_nube"
 
-# Dejamos que el sistema maneje la conexión de forma simple
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Motor configurado con parámetros de seguridad total para Render
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={
+        "sslmode": "require"
+    },
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
