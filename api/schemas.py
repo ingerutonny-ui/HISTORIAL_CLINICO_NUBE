42c17a73d23573230fb227813cdfd149d82b47be
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-04 15:54:49 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,31 +1,39 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
-# Esquemas para Pacientes (Necesarios para el Registro)
-class PacienteCreate(BaseModel):
+class PacienteBase(BaseModel):
     nombres: str
     apellidos: str
     documento_identidad: str
     codigo_paciente: str
 
-class Paciente(PacienteCreate):
+class PacienteCreate(PacienteBase):
+    pass
+
+class Paciente(PacienteBase):
     id: int
     class Config:
         from_attributes = True
 
-# Esquemas para Declaración Jurada
-class DeclaracionJuradaCreate(BaseModel):
+class DeclaracionJuradaBase(BaseModel):
     paciente_id: int
-    edad: Optional[int] = None
-    sexo: Optional[str] = None
-    lugar_nacimiento: Optional[str] = None
-    fecha_nacimiento: Optional[str] = None
-    estado_civil: Optional[str] = None
-    domicilio: Optional[str] = None
-    telefono: Optional[str] = None
-    profesion_oficio: Optional[str] = None
+    edad: str
+    sexo: str
+    fecha_nacimiento: str
+    lugar_nacimiento: str
+    domicilio: str
+    n_casa: str
+    zona_barrio: str
+    ciudad: str
+    pais: str
+    telefono: str
+    estado_civil: str
+    profesion_oficio: str
 
-class DeclaracionJurada(DeclaracionJuradaCreate):
+class DeclaracionJuradaCreate(DeclaracionJuradaBase):
+    pass
+
+class DeclaracionJurada(DeclaracionJuradaBase):
     id: int
     class Config:
         from_attributes = True
