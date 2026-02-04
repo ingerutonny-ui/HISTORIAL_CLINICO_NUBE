ecd06fb31ebc0f5ac09840613d09d08064a0ecd3
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-04 18:35:12 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -3,7 +3,6 @@ from . import models, schemas
 
 def create_paciente(db: Session, paciente: schemas.PacienteCreate):
     try:
-        # Usamos model_dump() para convertir los datos del schema a diccionario
         db_paciente = models.Paciente(**paciente.model_dump())
         db.add(db_paciente)
         db.commit()
@@ -20,6 +19,17 @@ def create_declaracion_p1(db: Session, declaracion: schemas.DeclaracionJuradaCre
         db.commit()
         db.refresh(db_declaracion)
         return db_declaracion
+    except Exception as e:
+        db.rollback()
+        raise e
+
+def create_antecedentes_p2(db: Session, antecedentes: schemas.AntecedentesP2Create):
+    try:
+        db_antecedentes = models.AntecedentesP2(**antecedentes.model_dump())
+        db.add(db_antecedentes)
+        db.commit()
+        db.refresh(db_antecedentes)
+        return db_antecedentes
     except Exception as e:
         db.rollback()
         raise e
