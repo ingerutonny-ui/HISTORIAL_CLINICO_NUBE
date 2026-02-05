f9b2f623ed3ee8e13701af796e5dc11f7decfce5
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-05 17:49:41 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -33,34 +33,8 @@ class FiliacionCreate(BaseModel):
 
 class AntecedentesCreate(BaseModel):
     paciente_id: int
-    p1: str = "INFO"; d1: Optional[str] = "NORMAL"
-    p2: str = "INFO"; d2: Optional[str] = "NORMAL"
-    p3: str = "INFO"; d3: Optional[str] = "NORMAL"
-    p4: str = "INFO"; d4: Optional[str] = "NORMAL"
-    p5: str = "INFO"; d5: Optional[str] = "NORMAL"
-    p6: str = "INFO"; d6: Optional[str] = "NORMAL"
-    p7: str = "INFO"; d7: Optional[str] = "NORMAL"
-    p8: str = "INFO"; d8: Optional[str] = "NORMAL"
-    p9: str = "INFO"; d9: Optional[str] = "NORMAL"
-    p10: str = "INFO"; d10: Optional[str] = "NORMAL"
-    p11: str = "INFO"; d11: Optional[str] = "NORMAL"
-    p12: str = "INFO"; d12: Optional[str] = "NORMAL"
-    p13: str = "INFO"; d13: Optional[str] = "NORMAL"
-    p14: str = "INFO"; d14: Optional[str] = "NINGUNA"
-    p15: str = "INFO"; d15: Optional[str] = "NINGUNA"
-    p16: str = "INFO"; d16: Optional[str] = "NINGUNA"
-    p17: str = "INFO"; d17: Optional[str] = "NINGUNA"
-    p18: str = "INFO"; d18: Optional[str] = "NINGUNA"
-    p19: str = "INFO"; d19: Optional[str] = "NINGUNA"
-    p20: str = "INFO"; d20: Optional[str] = "NINGUNA"
-    p21: str = "INFO"; d21: Optional[str] = "NINGUNA"
-    p22: str = "INFO"; d22: Optional[str] = "NINGUNA"
-
-class HabitosCreate(BaseModel):
-    paciente_id: int
-    fuma: str = "NO"
-    bebe: str = "NO"
-    drogas: str = "NO"
-    coca: str = "NO"
-    deportes: Optional[str] = "NINGUNO"
-    grupo_sanguineo: Optional[str] = "S/D"
+    p1: str; d1: str; p2: str; d2: str; p3: str; d3: str; p4: str; d4: str; p5: str; d5: str
+    p6: str; d6: str; p7: str; d7: str; p8: str; d8: str; p9: str; d9: str; p10: str; d10: str
+    p11: str; d11: str; p12: str; d12: str; p13: str; d13: str; p14: str; d14: str; p15: str; d15: str
+    p16: str; d16: str; p17: str; d17: str; p18: str; d18: str; p19: str; d19: str; p20: str; d20: str
+    p21: str; d21: str; p22: str; d22: str
