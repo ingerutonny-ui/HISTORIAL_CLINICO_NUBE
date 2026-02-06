f9a4a0fada6085b32d5950386c50c97de4166ec9
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-06 14:07:42 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,48 +1,48 @@
-from sqlalchemy import Column, Integer, str, Date, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Text
 from .database import Base
 
 class Paciente(Base):
     __tablename__ = "pacientes"
     id = Column(Integer, primary_key=True, index=True)
-    nombres = Column(str)
-    apellidos = Column(str)
-    ci = Column(str, unique=True, index=True)
-    codigo_paciente = Column(str)
+    nombres = Column(String)
+    apellidos = Column(String)
+    ci = Column(String, unique=True, index=True)
+    codigo_paciente = Column(String)
 
 class DeclaracionJurada(Base):
     __tablename__ = "filiacion"
     id = Column(Integer, primary_key=True, index=True)
     paciente_id = Column(Integer, ForeignKey("pacientes.id"))
     edad = Column(Integer)
-    sexo = Column(str)
+    sexo = Column(String)
     fecha_nacimiento = Column(Date)
-    lugar_nacimiento = Column(str)
-    domicilio = Column(str)
-    n_casa = Column(str)
-    zona_barrio = Column(str)
-    ciudad = Column(str)
-    pais = Column(str)
-    telefono = Column(str)
-    estado_civil = Column(str)
-    profesion_oficio = Column(str)
+    lugar_nacimiento = Column(String)
+    domicilio = Column(String)
+    n_casa = Column(String)
+    zona_barrio = Column(String)
+    ciudad = Column(String)
+    pais = Column(String)
+    telefono = Column(String)
+    estado_civil = Column(String)
+    profesion_oficio = Column(String)
 
 class AntecedentesP2(Base):
     __tablename__ = "antecedentes_p2"
     id = Column(Integer, primary_key=True, index=True)
     paciente_id = Column(Integer, ForeignKey("pacientes.id"))
-    p1 = Column(str); d1 = Column(str); p2 = Column(str); d2 = Column(str); p3 = Column(str); d3 = Column(str)
-    p4 = Column(str); d4 = Column(str); p5 = Column(str); d5 = Column(str); p6 = Column(str); d6 = Column(str)
-    p7 = Column(str); d7 = Column(str); p8 = Column(str); d8 = Column(str); p9 = Column(str); d9 = Column(str)
-    p10 = Column(str); d10 = Column(str); p11 = Column(str); d11 = Column(str); p12 = Column(str); d12 = Column(str)
-    p13 = Column(str); d13 = Column(str); p14 = Column(str); d14 = Column(str); p15 = Column(str); d15 = Column(str)
-    p16 = Column(str); d16 = Column(str); p17 = Column(str); d17 = Column(str); p18 = Column(str); d18 = Column(str)
+    p1 = Column(String); d1 = Column(String); p2 = Column(String); d2 = Column(String); p3 = Column(String); d3 = Column(String)
+    p4 = Column(String); d4 = Column(String); p5 = Column(String); d5 = Column(String); p6 = Column(String); d6 = Column(String)
+    p7 = Column(String); d7 = Column(String); p8 = Column(String); d8 = Column(String); p9 = Column(String); d9 = Column(String)
+    p10 = Column(String); d10 = Column(String); p11 = Column(String); d11 = Column(String); p12 = Column(String); d12 = Column(String)
+    p13 = Column(String); d13 = Column(String); p14 = Column(String); d14 = Column(String); p15 = Column(String); d15 = Column(String)
+    p16 = Column(String); d16 = Column(String); p17 = Column(String); d17 = Column(String); p18 = Column(String); d18 = Column(String)
 
 class HabitosRiesgosP3(Base):
     __tablename__ = "habitos_p3"
     id = Column(Integer, primary_key=True, index=True)
     paciente_id = Column(Integer, ForeignKey("pacientes.id"))
-    h1 = Column(str); r1 = Column(str); h2 = Column(str); r2 = Column(str); h3 = Column(str); r3 = Column(str)
-    h4 = Column(str); r4 = Column(str); h5 = Column(str); r5 = Column(str); h6 = Column(str); r6 = Column(str)
-    h7 = Column(str); r7 = Column(str); h8 = Column(str); r8 = Column(str); h9 = Column(str); r9 = Column(str)
-    h10 = Column(str); r10 = Column(str)
-    historia_laboral = Column(Text) # Aquí se guarda el JSON de la tabla
+    h1 = Column(String); r1 = Column(String); h2 = Column(String); r2 = Column(String); h3 = Column(String); r3 = Column(String)
+    h4 = Column(String); r4 = Column(String); h5 = Column(String); r5 = Column(String); h6 = Column(String); r6 = Column(String)
+    h7 = Column(String); r7 = Column(String); h8 = Column(String); r8 = Column(String); h9 = Column(String); r9 = Column(String)
+    h10 = Column(String); r10 = Column(String)
+    historia_laboral = Column(Text)
