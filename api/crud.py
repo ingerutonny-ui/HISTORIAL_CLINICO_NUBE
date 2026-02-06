f7864dcb46ba3a37670e1f031e949f2a40586c0f
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-06 14:41:04 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -1,37 +1,43 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
+# Obtener lista de pacientes para la consulta en nube
 def get_pacientes(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Paciente).offset(skip).limit(limit).all()
 
+# Crear Paciente (Parte Inicial)
 def create_paciente(db: Session, paciente: schemas.PacienteCreate):
-    db_paciente = models.Paciente(**paciente.dict())
+    db_paciente = models.Paciente(**paciente.model_dump())
     db.add(db_paciente)
     db.commit()
     db.refresh(db_paciente)
     return db_paciente
 
+# Crear Filiación (Parte 1)
 def create_filiacion(db: Session, filiacion: schemas.FiliacionCreate):
-    db_filiacion = models.DeclaracionJurada(**filiacion.dict())
+    db_filiacion = models.DeclaracionJurada(**filiacion.model_dump())
     db.add(db_filiacion)
     db.commit()
     db.refresh(db_filiacion)
     return db_filiacion
 
+# Crear Antecedentes (Parte 2)
 def create_antecedentes(db: Session, antecedentes: schemas.AntecedentesCreate):
-    db_ante = models.AntecedentesP2(**antecedentes.dict())
+    db_ante = models.AntecedentesP2(**antecedentes.model_dump())
     db.add(db_ante)
     db.commit()
     db.refresh(db_ante)
     return db_ante
 
+# Crear Hábitos y Riesgos (Parte 3)
 def create_habitos(db: Session, habitos: schemas.HabitosCreate):
-    db_hab = models.HabitosRiesgosP3(**habitos.dict())
+    db_hab = models.HabitosRiesgosP3(**habitos.model_dump())
     db.add(db_hab)
     db.commit()
     db.refresh(db_hab)
     return db_hab
 
+# Obtener todo para el PDF
 def get_historial_completo(db: Session, paciente_id: int):
     paciente = db.query(models.Paciente).filter(models.Paciente.id == paciente_id).first()
     filiacion = db.query(models.DeclaracionJurada).filter(models.DeclaracionJurada.paciente_id == paciente_id).first()
