59221de0f8fbf52cb519561bdf5d57e457d8c718
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-06 22:10:05 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -32,13 +32,14 @@ class AntecedentesP2(Base):
     paciente_id = Column(Integer, ForeignKey("pacientes.id"))
     p1 = Column(String); d1 = Column(String); p2 = Column(String); d2 = Column(String)
     p3 = Column(String); d3 = Column(String); p4 = Column(String); d4 = Column(String)
-    p5 = Column(String); d5 = Column(String); p6 = Column(String); d6 = Column(String)
-    p7 = Column(String); d7 = Column(String); p8 = Column(String); d8 = Column(String)
-    p9 = Column(String); d9 = Column(String); p10 = Column(String); d10 = Column(String)
-    p11 = Column(String); d11 = Column(String); p12 = Column(String); d12 = Column(String)
-    p13 = Column(String); d13 = Column(String); p14 = Column(String); d14 = Column(String)
-    p15 = Column(String); d15 = Column(String); p16 = Column(String); d16 = Column(String)
-    p17 = Column(String); d17 = Column(String); p18 = Column(String); d18 = Column(String)
+    p5 = Column(String); d5 = Column(String) # ... hasta p18 y d18
+    p6 = Column(String); d6 = Column(String); p7 = Column(String); d7 = Column(String)
+    p8 = Column(String); d8 = Column(String); p9 = Column(String); d9 = Column(String)
+    p10 = Column(String); d10 = Column(String); p11 = Column(String); d11 = Column(String)
+    p12 = Column(String); d12 = Column(String); p13 = Column(String); d13 = Column(String)
+    p14 = Column(String); d14 = Column(String); p15 = Column(String); d15 = Column(String)
+    p16 = Column(String); d16 = Column(String); p17 = Column(String); d17 = Column(String)
+    p18 = Column(String); d18 = Column(String)
 
 class HabitosRiesgosP3(Base):
     __tablename__ = "habitos_p3"
@@ -52,4 +53,4 @@ class HabitosRiesgosP3(Base):
     medicamentos_detalle = Column(String)
     grupo_sanguineo = Column(String)
     historia_laboral = Column(String)
-    riesgos_vida_laboral = Column(String) # Campo recuperado
+    riesgos_vida_laboral = Column(String) # CAMPO CRÍTICO
