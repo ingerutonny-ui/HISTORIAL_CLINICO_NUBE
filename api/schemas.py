aaa8ccc1c4a9d171d6ff664947ea31cd65baf733
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-07 00:22:13 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,18 +1,31 @@
 from pydantic import BaseModel, ConfigDict
-from typing import Optional
+from typing import Optional, List
 from datetime import date
 
 class PacienteCreate(BaseModel):
-    nombres: str; apellidos: str; ci: str; codigo_paciente: str
+    nombres: str
+    apellidos: str
+    ci: str
+    codigo_paciente: str
 
 class Paciente(PacienteCreate):
     id: int
     model_config = ConfigDict(from_attributes=True)
 
 class FiliacionCreate(BaseModel):
-    paciente_id: int; edad: int; sexo: str; fecha_nacimiento: date
-    estado_civil: str; lugar_nacimiento: str; domicilio: str; n_casa: str
-    zona_barrio: str; ciudad: str; pais: str; telefono: str; profesion_oficio: str
+    paciente_id: int
+    edad: int
+    sexo: str
+    fecha_nacimiento: date
+    estado_civil: str
+    lugar_nacimiento: str
+    domicilio: str
+    n_casa: str
+    zona_barrio: str
+    ciudad: str
+    pais: str
+    telefono: str
+    profesion_oficio: str
 
 class AntecedentesCreate(BaseModel):
     paciente_id: int
@@ -23,13 +36,19 @@ class AntecedentesCreate(BaseModel):
 
 class HabitosCreate(BaseModel):
     paciente_id: int
-    fuma_si_no: str; fuma_detalle: str
-    alcohol_si_no: str; alcohol_detalle: str
-    drogas_si_no: str; drogas_detalle: str
+    fuma_si_no: str
+    fuma_detalle: str
+    alcohol_si_no: str
+    alcohol_detalle: str
+    drogas_si_no: str
+    drogas_detalle: str
     pijchar_si_no: str
-    deportes_si_no: str; deportes_detalle: str
+    deportes_si_no: str
+    deportes_detalle: str
     grupo_sanguineo: str
-    accidentes_si_no: str; accidentes_detalle: str
-    medicamentos_si_no: str; medicamentos_detalle: str
+    accidentes_si_no: str
+    accidentes_detalle: str
+    medicamentos_si_no: str
+    medicamentos_detalle: str
     riesgos_vida_laboral: str
     historia_laboral: str
