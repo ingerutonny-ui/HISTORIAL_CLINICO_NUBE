4376297a33957540da50560e9fd1945a997fcc64
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-06 23:03:08 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -1,9 +1,6 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-def get_pacientes(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Paciente).offset(skip).limit(limit).all()
-
 def create_paciente(db: Session, paciente: schemas.PacienteCreate):
     db_paciente = models.Paciente(**paciente.model_dump())
     db.add(db_paciente)
@@ -11,35 +8,31 @@ def create_paciente(db: Session, paciente: schemas.PacienteCreate):
     db.refresh(db_paciente)
     return db_paciente
 
+def get_pacientes(db: Session):
+    return db.query(models.Paciente).all()
+
 def create_filiacion(db: Session, filiacion: schemas.FiliacionCreate):
     db_filiacion = models.DeclaracionJurada(**filiacion.model_dump())
     db.add(db_filiacion)
     db.commit()
-    db.refresh(db_filiacion)
     return db_filiacion
 
 def create_antecedentes(db: Session, antecedentes: schemas.AntecedentesCreate):
-    db_ante = models.AntecedentesP2(**antecedentes.model_dump())
-    db.add(db_ante)
+    db_ant = models.AntecedentesP2(**antecedentes.model_dump())
+    db.add(db_ant)
     db.commit()
-    db.refresh(db_ante)
-    return db_ante
+    return db_ant
 
 def create_habitos(db: Session, habitos: schemas.HabitosCreate):
     db_hab = models.HabitosRiesgosP3(**habitos.model_dump())
     db.add(db_hab)
     db.commit()
-    db.refresh(db_hab)
     return db_hab
 
 def get_historial_completo(db: Session, paciente_id: int):
-    paciente = db.query(models.Paciente).filter(models.Paciente.id == paciente_id).first()
-    filiacion = db.query(models.DeclaracionJurada).filter(models.DeclaracionJurada.paciente_id == paciente_id).first()
-    antecedentes = db.query(models.AntecedentesP2).filter(models.AntecedentesP2.paciente_id == paciente_id).first()
-    habitos = db.query(models.HabitosRiesgosP3).filter(models.HabitosRiesgosP3.paciente_id == paciente_id).first()
     return {
-        "paciente": paciente,
-        "filiacion": filiacion,
-        "antecedentes": antecedentes,
-        "habitos": habitos
+        "paciente": db.query(models.Paciente).filter(models.Paciente.id == paciente_id).first(),
+        "filiacion": db.query(models.DeclaracionJurada).filter(models.DeclaracionJurada.paciente_id == paciente_id).first(),
+        "antecedentes": db.query(models.AntecedentesP2).filter(models.AntecedentesP2.paciente_id == paciente_id).first(),
+        "habitos": db.query(models.HabitosRiesgosP3).filter(models.HabitosRiesgosP3.paciente_id == paciente_id).first()
     }
