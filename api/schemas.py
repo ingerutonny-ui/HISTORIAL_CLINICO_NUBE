495cb4af3f772d085a884afe08c7f62bbd929803
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-08 22:50:52 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -16,16 +16,14 @@ class FiliacionCreate(BaseModel):
 
 class AntecedentesCreate(BaseModel):
     paciente_id: int
-    vista: str; auditivo: str; respiratorio: str; cardio: str; estomago: str
-    sangre: str; genito: str; nervioso: str; psiquiatrico: str; osteo: str
-    endocrino: str; reumato: str; generales: str; dermato: str; alergias: str
-    infecciosas: str; cirugias: str; acc_trabajo: str; acc_personales: str
-    medicamentos: str; familiares: str; otros: str
+    vista: str; auditivo: str; respiratorio: str; cardio: str; estomago: str; sangre: str
+    genito: str; nervioso: str; psiquiatrico: str; osteo: str; endocrino: str; reumato: str
+    generales: str; dermato: str; alergias: str; infecciosas: str; cirugias: str
+    acc_trabajo: str; acc_personales: str; medicamentos: str; familiares: str; otros: str
 
 class HabitosCreate(BaseModel):
-    paciente_id: int; fuma_si_no: str; fuma_detalle: str; alcohol_si_no: str
-    alcohol_detalle: str; drogas_si_no: str; drogas_detalle: str; pijchar_si_no: str
-    deportes_si_no: str; deportes_detalle: str; grupo_sanguineo: str
-    accidentes_si_no: str; accidentes_detalle: str; medicamentos_si_no: str
-    medicamentos_detalle: str; alergias_si_no: str; alergias_detalle: str
+    paciente_id: int; fuma_si_no: str; fuma_detalle: str; alcohol_si_no: str; alcohol_detalle: str
+    drogas_si_no: str; drogas_detalle: str; pijchar_si_no: str; deportes_si_no: str
+    deportes_detalle: str; grupo_sanguineo: str; accidentes_si_no: str; accidentes_detalle: str
+    medicamentos_si_no: str; medicamentos_detalle: str; alergias_si_no: str; alergias_detalle: str
     riesgos_vida_laboral: str; historia_laboral: str
