9310455cbff0e2c68c9e790a08ae87ddcd3680bc
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-09 00:10:49 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,29 +1,53 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 from typing import Optional
-from datetime import date
 
 class PacienteCreate(BaseModel):
-    nombres: str; apellidos: str; ci: str; codigo_paciente: str
+    nombre: str
+    apellido: str
+    ci: str
+    codigo_paciente: str
 
 class Paciente(PacienteCreate):
     id: int
-    model_config = ConfigDict(from_attributes=True)
+    class Config:
+        orm_mode = True
 
 class FiliacionCreate(BaseModel):
-    paciente_id: int; edad: int; sexo: str; fecha_nacimiento: date; estado_civil: str
-    lugar_nacimiento: str; domicilio: str; n_casa: str; zona_barrio: str; ciudad: str
-    pais: str; telefono: str; profesion_oficio: str
+    paciente_id: int
+    edad: int
+    sexo: str
+    fecha_nacimiento: str
+    lugar_nacimiento: str
+    domicilio: str
+    n_casa: str
+    zona_barrio: str
+    ciudad: str
+    pais: str
+    telefono: str
+    estado_civil: str
+    profesion_oficio: str
 
 class AntecedentesCreate(BaseModel):
     paciente_id: int
-    vista: str; auditivo: str; respiratorio: str; cardio: str; estomago: str; sangre: str
-    genito: str; nervioso: str; psiquiatrico: str; osteo: str; endocrino: str; reumato: str
-    generales: str; dermato: str; alergias: str; infecciosas: str; cirugias: str
-    acc_trabajo: str; acc_personales: str; medicamentos: str; familiares: str; otros: str
-
-class HabitosCreate(BaseModel):
-    paciente_id: int; fuma_si_no: str; fuma_detalle: str; alcohol_si_no: str; alcohol_detalle: str
-    drogas_si_no: str; drogas_detalle: str; pijchar_si_no: str; deportes_si_no: str
-    deportes_detalle: str; grupo_sanguineo: str; accidentes_si_no: str; accidentes_detalle: str
-    medicamentos_si_no: str; medicamentos_detalle: str; alergias_si_no: str; alergias_detalle: str
-    riesgos_vida_laboral: str; historia_laboral: str
+    vista: str
+    auditivo: str
+    respiratorio: str
+    cardio: str
+    digestivos: str
+    sangre: str
+    genitourinario: str
+    sistema_nervioso: str
+    psiquiatricos: str
+    osteomusculares: str
+    endocrino: str
+    alergias: str
+    cirugias: str
+    acc_trabajo: str
+    acc_personales: str
+    medicamentos: str
+    infecciosas: str
+    ap_urinario: str
+    linfatico: str
+    reumatologicos: str
+    otros: str
+    generales: str
