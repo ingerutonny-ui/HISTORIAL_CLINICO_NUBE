905ba1adbd5a417ce3322d77411acda869fdb00f
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-09 01:00:55 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Configuración para Render con SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
