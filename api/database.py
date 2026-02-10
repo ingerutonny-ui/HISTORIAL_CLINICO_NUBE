5b4ef9a45391bf9e7e2fe7deaf7e0b81a9a871f1
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-10 17:03:56 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,20 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Ruta exacta basada en tu Mount Path configurado (/data)
-DB_PATH = "/data/historial.db"
-
-# Asegurar que el directorio existe
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+# Usamos SQLite local en el servidor de Render
+SQLALCHEMY_DATABASE_URL = "sqlite:///./historial.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
