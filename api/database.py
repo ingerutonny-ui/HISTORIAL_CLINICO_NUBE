174b877dcd0364e62fcaceaa2f68fc174973cb56
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-11 00:28:49 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -3,10 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Forzar la ruta al volumen persistente de Render
+# Ruta absoluta al DISK persistente
 SQLALCHEMY_DATABASE_URL = "sqlite:////data/historial.db"
 
-# Solo usa local si /data no existe (para que no falle al subir)
 if not os.path.exists("/data"):
     SQLALCHEMY_DATABASE_URL = "sqlite:///./historial.db"
 
