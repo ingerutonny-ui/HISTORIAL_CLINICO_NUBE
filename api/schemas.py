9f211e1ba1b574338fbc746e0c7e739d36554998
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-10 23:40:16 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -10,16 +10,16 @@ class PacienteCreate(BaseModel):
 
 class FiliacionCreate(BaseModel):
     paciente_id: int
-    edad: int
-    sexo: str
-    fecha_nacimiento: str
-    lugar_nacimiento: str
-    domicilio: str
-    n_casa: str
-    zona_barrio: str
-    ciudad: str
-    pais: str
-    telefono: str
-    estado_civil: str
-    profesion_oficio: str
+    edad: Optional[Any] = None
+    sexo: Optional[Any] = None
+    fecha_nacimiento: Optional[Any] = None
+    lugar_nacimiento: Optional[Any] = None
+    domicilio: Optional[Any] = None
+    n_casa: Optional[Any] = None
+    zona_barrio: Optional[Any] = None
+    ciudad: Optional[Any] = None
+    pais: Optional[Any] = None
+    telefono: Optional[Any] = None
+    estado_civil: Optional[Any] = None
+    profesion_oficio: Optional[Any] = None
     model_config = ConfigDict(from_attributes=True, extra='allow')
