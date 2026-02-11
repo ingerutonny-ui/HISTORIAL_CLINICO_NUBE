d0c327c6a425698bce658c7b3c8d83530b38e562
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-11 00:08:17 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -3,11 +3,16 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Ruta absoluta al DISK persistente de Render
 SQLALCHEMY_DATABASE_URL = "sqlite:////data/historial.db"
+
 if not os.path.exists("/data"):
     SQLALCHEMY_DATABASE_URL = "sqlite:///./historial.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 30})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False, "timeout": 30}
+)
 
 @event.listens_for(engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
