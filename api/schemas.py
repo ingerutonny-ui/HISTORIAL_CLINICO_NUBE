32b999749a0f0109de06dcf75ea16fd2f6378994
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-10 23:32:09 -0400

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -2,24 +2,24 @@ from pydantic import BaseModel, ConfigDict
 from typing import Optional, Any
 
 class PacienteCreate(BaseModel):
-    nombre: Any
-    apellido: Any
-    ci: Any
-    codigo_paciente: Any
-    model_config = ConfigDict(extra='allow')
+    nombre: str
+    apellido: str
+    ci: str
+    codigo_paciente: str
+    model_config = ConfigDict(from_attributes=True, extra='allow')
 
 class FiliacionCreate(BaseModel):
     paciente_id: int
-    edad: Optional[Any] = None
-    sexo: Optional[Any] = None
-    fecha_nacimiento: Optional[Any] = None
-    lugar_nacimiento: Optional[Any] = None
-    domicilio: Optional[Any] = None
-    n_casa: Optional[Any] = None
-    zona_barrio: Optional[Any] = None
-    ciudad: Optional[Any] = None
-    pais: Optional[Any] = None
-    telefono: Optional[Any] = None
-    estado_civil: Optional[Any] = None
-    profesion_oficio: Optional[Any] = None
-    model_config = ConfigDict(extra='allow')
+    edad: int
+    sexo: str
+    fecha_nacimiento: str
+    lugar_nacimiento: str
+    domicilio: str
+    n_casa: str
+    zona_barrio: str
+    ciudad: str
+    pais: str
+    telefono: str
+    estado_civil: str
+    profesion_oficio: str
+    model_config = ConfigDict(from_attributes=True, extra='allow')
