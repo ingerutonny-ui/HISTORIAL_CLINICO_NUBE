b83f7edecd90c09ef029e75400f153c6dbba43dc
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-22 09:51:42 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -1,99 +1,35 @@
 from sqlalchemy.orm import Session
 from . import models
 
-def get_paciente_by_ci(db: Session, ci: str):
-    return db.query(models.Paciente).filter(models.Paciente.ci == ci).first()
-
-def create_paciente(db: Session, data: dict):
-    # Verificar si el CI ya existe para evitar Error 500
-    existente = db.query(models.Paciente).filter(models.Paciente.ci == data.get("ci")).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
-        db.commit()
-        db.refresh(existente)
-        return existente
-    
-    db_obj = models.Paciente(**data)
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-    return db_obj
-
 def upsert_filiacion(db: Session, data: dict):
-    p_id = data.get("paciente_id")
-    existente = db.query(models.DeclaracionJurada).filter(models.DeclaracionJurada.paciente_id == p_id).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
+    obj = db.query(models.Paciente).filter(models.Paciente.id == data['id']).first()
+    if obj:
+        for k, v in data.items(): setattr(obj, k, v)
     else:
-        existente = models.DeclaracionJurada(**data)
-        db.add(existente)
+        obj = models.Paciente(**data)
+        db.add(obj)
     db.commit()
-    db.refresh(existente)
-    return existente
+    db.refresh(obj)
+    return obj
 
 def upsert_p2(db: Session, data: dict):
-    p_id = data.get("paciente_id")
-    existente = db.query(models.AntecedentesP2).filter(models.AntecedentesP2.paciente_id == p_id).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
+    obj = db.query(models.AntecedentesP2).filter(models.AntecedentesP2.paciente_id == data['paciente_id']).first()
+    if obj:
+        for k, v in data.items(): setattr(obj, k, v)
     else:
-        existente = models.AntecedentesP2(**data)
-        db.add(existente)
+        obj = models.AntecedentesP2(**data)
+        db.add(obj)
     db.commit()
-    db.refresh(existente)
-    return existente
+    db.refresh(obj)
+    return obj
 
 def upsert_p3(db: Session, data: dict):
-    p_id = data.get("paciente_id")
-    existente = db.query(models.HabitosRiesgosP3).filter(models.HabitosRiesgosP3.paciente_id == p_id).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
+    obj = db.query(models.HabitosRiesgosP3).filter(models.HabitosRiesgosP3.paciente_id == data['paciente_id']).first()
+    if obj:
+        for k, v in data.items(): setattr(obj, k, v)
     else:
-        existente = models.HabitosRiesgosP3(**data)
-        db.add(existente)
+        obj = models.HabitosRiesgosP3(**data)
+        db.add(obj)
     db.commit()
-    db.refresh(existente)
-    return existente
-
-def create_enfermera(db: Session, data: dict):
-    ci = data.get("ci_enfe")
-    existente = db.query(models.Enfermera).filter(models.Enfermera.ci_enfe == ci).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
-    else:
-        existente = models.Enfermera(**data)
-        db.add(existente)
-    db.commit()
-    db.refresh(existente)
-    return existente
-
-def create_doctor(db: Session, data: dict):
-    ci = data.get("ci_doc")
-    existente = db.query(models.Doctor).filter(models.Doctor.ci_doc == ci).first()
-    if existente:
-        for key, value in data.items():
-            setattr(existente, key, value)
-    else:
-        existente = models.Doctor(**data)
-        db.add(existente)
-    db.commit()
-    db.refresh(existente)
-    return existente
-
-def delete_paciente(db: Session, paciente_id: int):
-    # Borrado manual de hijos para asegurar limpieza total antes del padre
-    db.query(models.DeclaracionJurada).filter(models.DeclaracionJurada.paciente_id == paciente_id).delete()
-    db.query(models.AntecedentesP2).filter(models.AntecedentesP2.paciente_id == paciente_id).delete()
-    db.query(models.HabitosRiesgosP3).filter(models.HabitosRiesgosP3.paciente_id == paciente_id).delete()
-    
-    db_obj = db.query(models.Paciente).filter(models.Paciente.id == paciente_id).first()
-    if db_obj:
-        db.delete(db_obj)
-        db.commit()
-        return True
-    return False
+    db.refresh(obj)
+    return obj
