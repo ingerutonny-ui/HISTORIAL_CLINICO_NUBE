b18ad9fa949135979e4026ab2ea60119e598c1a2
ingerutonny-ui <ingerutonny@gmail.com>
2026-02-23 01:00:50 -0400

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -33,3 +33,29 @@ def upsert_p3(db: Session, data: dict):
     db.commit()
     db.refresh(obj)
     return obj
+
+# --- NUEVAS FUNCIONES PARA PERSONAL MÉDICO ---
+
+def upsert_doctor(db: Session, data: dict):
+    """Busca por CI; si existe actualiza, si no crea un nuevo Doctor"""
+    obj = db.query(models.Doctor).filter(models.Doctor.ci_doc == data['ci_doc']).first()
+    if obj:
+        for k, v in data.items(): setattr(obj, k, v)
+    else:
+        obj = models.Doctor(**data)
+        db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
+
+def upsert_enfermera(db: Session, data: dict):
+    """Busca por CI; si existe actualiza, si no crea una nueva Enfermera"""
+    obj = db.query(models.Enfermera).filter(models.Enfermera.ci_enfe == data['ci_enfe']).first()
+    if obj:
+        for k, v in data.items(): setattr(obj, k, v)
+    else:
+        obj = models.Enfermera(**data)
+        db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
