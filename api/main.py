e6484d027130b4999c7ddd60854288765805507f
ingerutonny-ui <255609125+ingerutonny-ui@users.noreply.github.com>
2026-05-22 23:28:55 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from .database import SessionLocal, engine, Base
+from . import crud, models
+
+# Crear tablas en la base de datos
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Configuración CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -10,6 +17,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# RUTA DE SALUD
 @app.get("/")
-def keep_alive():
+def health_check():
     return {"status": "ok"}
+
+# RUTA LISTA PACIENTES
+@app.get("/pacientes/")
+def read_pacientes(db: Session = Depends(get_db)):
+    return db.query(models.Paciente).all()
