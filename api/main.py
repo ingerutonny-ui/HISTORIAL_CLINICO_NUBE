78f24573219e529dfb35c3775c6efccb87037a7f
ingerutonny-ui <255609125+ingerutonny-ui@users.noreply.github.com>
2026-05-22 23:16:12 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from .database import SessionLocal, engine, Base
-from . import models
-
-# Crear tablas
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -16,18 +10,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# RUTA PARA MANTENER EL SERVIDOR VIVO Y QUE RENDER NO LA MATE
 @app.get("/")
-def health_check():
-    return {"status": "ok"}
-
-@app.get("/pacientes/")
-def read_pacientes(db: Session = Depends(get_db)):
-    return db.query(models.Paciente).all()
+def read_root():
+    return {"message": "Servidor estable"}
